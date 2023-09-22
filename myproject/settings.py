fc86961f027c9e98de2b872962983da7ba5b1d55
Haresh <vegadharesh32@gmail.com>
2023-09-22 11:15:14 +0530

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dj_database_url
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k80(v0i&dc!s2ih8re@7-1+y!a@7()xu)ym9*5ir)pqsuwh(&h'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -81,6 +83,8 @@ DATABASES = {
     }
 }
 
+databases_url = os.environ.get("DATABASES_URL")
+DATABASES['default'] = dj_database_url.parse(databases_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
